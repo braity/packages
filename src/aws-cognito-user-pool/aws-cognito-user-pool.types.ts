cef51dd0a206b79374a8f34418a8daddd9a1fbb8
Kacper Obrzut <kacper.obrzut@zaven.pl>
2023-11-08 21:34:48 +0100

    docs(aws-cognito-user-pool): add jsdoc for IAWSCognitoUserPool

--- src/aws-cognito-user-pool/aws-cognito-user-pool.types.ts
+++ src/aws-cognito-user-pool/aws-cognito-user-pool.types.ts
@@ -12,16 +12,55 @@ export type AWSCognitoUserPoolForUserPoolIdOptions = {
   }
 }
 
+/**
+ * @template T
+ * AWS Cognito UserPool wrapper.
+ * You can use generic type to define additional user attributes.
+ */
 export interface IAWSCognitoUserPool<T extends Properties> {
+
+  /**
+   * Creates a new user in AWS Cognito User Pool
+   * @param {CreateUserFields<T>|{object}} properties user properties
+   * @param {string} properties.email user email
+   * @param {string} properties.password user temporary password
+   * @param {AddUserOptions|object} [options] options
+   * @param {boolean} [options.verified] sets user verified and doesn't send email with password
+   * @returns {Promise<User<T>>} user data
+   */
   createUser(properties?: CreateUserFields<T>, options?: AddUserOptions): Promise<User<T>>
 
+  /**
+   * Updates user
+   * @param {string} id identifier (username) from Cognito
+   * @param {UpdateUserFields<T>|object} fields fields to update
+   * @param {string} [fields.password] password to update. User needs to change it after log in.
+   * @returns {Promise<User<T>>}} user data
+   */
   updateUser(id: string, fields: UpdateUserFields<T>): Promise<User<T>>
 
+  /**
+   * @param {string} id identifier (username) from Cognito
+   * @returns {Promise<void>}
+   */
   deleteUser(id: string): Promise<void>
 
+  /**
+   * Returns a list of all users
+   * @returns {Promise<User<T>[]>} list of users
+   */
   listUsers(): Promise<User<T>[]>
 
+  /**
+   * Gets user by identifier (username)
+   * @param {string} id identifier (username) from Cognito
+   * @returns {Promise<User<T>>} user data
+   */
   getUserById(id: string): Promise<User<T>>
 
+  /**
+   * @param {string} token
+   * @returns {Promise<User<T>>} user data
+   */
   getUserUsingToken(token: string): Promise<User<T>>
 }
